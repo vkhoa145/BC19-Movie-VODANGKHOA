df9f0a389ddb1a0716b5cc3201ddb8d6271f07a1
Dang Khoa <khoavodang1451997@gmail.com>
2022-07-13 21:02:45 +0700

    loi khong start

--- src/modules/Home/components/MovieShowing.js
+++ src/modules/Home/components/MovieShowing.js
@@ -17,14 +17,14 @@ const MovieShowing = () => {
       {data?.map((movie, index) => {
         return (
           <div key={index} className="card rounded mx-2.5 my-5" style={{ width: '18rem' }}>
-            {/* <img className="card-img-top" src={movie.hinhAnh} alt={movie.tenPhim} />
+            <img className="card-img-top" src={movie.hinhAnh} alt={movie.tenPhim} />
             <div className="card-body">
               <h5 className="card-title movie-title">{movie.tenPhim}</h5>
               <p className="card-text">{movie.moTa}</p>
               <a href="#" onClick={() => navigate(`/movies/${movie.maPhim}`)} className="btn btn-primary">Details</a>
-            <p>hello</p>
-            </div> */}
-            <MultipleRows movie={movie}/>
+            
+            </div>
+            {/* <MultipleRows movie={movie}/> */}
           </div>
 
         )
